4a10c9ac3eebd7629aaf0784c530364530710b8c
mo7amedaliEbaid <mo7amedaliebaid@gmail.com>
2023-07-25 04:17:34 +0300

    commit3

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
